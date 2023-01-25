dfefc669024e29cab7d61df8951bb7cf4a1ddfae
HuseynovNuru <nurumh@code.edu.az>
2023-01-25 19:44:24 +0400

    Class metodu

---
+++ HomeTask7/HomeTask7/Laptop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask7
+{
+    internal class Laptop
+    {
+
+        public string Brand;
+        public string Model;
+        public int Price;
+        
+        
+        public Laptop(string brand, string model, int price = 0)
+        {
+            Price = price;
+            Brand = brand;
+            Model = model;
+        }
+
+
+        public string ShowInfo()
+        {
+            return $"Brand {Brand}, Model {Model}, Price {Price} ";
+        }
+    }
+   
+}
---
+++ HomeTask7/HomeTask7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HomeTask7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop notebook = new Laptop("Asus", "ROG Strix",2600);
+            notebook.Price = 2600;
+
+            Laptop[] laptops = new Laptop[2];
+            for (int i = 0; i < laptops.Length; i++)
+            {
+                Console.WriteLine("Enter Brand:");
+                string brand = Console.ReadLine();
+
+                Console.WriteLine("Enter Model:");
+                string model = Console.ReadLine();
+
+                Console.WriteLine("Enter Price:");
+                string priceStr = Console.ReadLine();
+                int price = Convert.ToInt32(priceStr);
+
+                Laptop lp = new Laptop(brand, model, price);
+
+                laptops[i] = lp;
+
+            }
+            double sum = 0;
+            for (int i = 0; i < laptops.Length; i++)
+            {
+                sum += laptops[i].Price;
+            }
+            double average = sum / laptops.Length;
+            Console.WriteLine(average);
+        }
+    }
+}
